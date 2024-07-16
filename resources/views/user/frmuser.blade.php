f3dd79fbd1e2d59014bef715e3a5180b684dc4dc
Alan-Arias <74990048+Alan-Arias@users.noreply.github.com>
2024-07-16 05:02:22 -0400

    Update frmuser.blade.php

--- resources/views/user/frmuser.blade.php
+++ resources/views/user/frmuser.blade.php
@@ -52,36 +52,9 @@
     </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><i class="fas fa-cogs"></i> Taller Servimag</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">                    
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/users') }}"><i class="fas fa-car me-1"></i>Gestionar Usuarios</a>
-                </li>                
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/trabajadors') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Trabajadores</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/servicios') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Servicios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/GestionarVehiculos') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Vehiculos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/detalle') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Detalles Vehiculo</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/reserva') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Reservas</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+@extends('app3')
+
+@section('content')
 
 <div class="container">
     <h2>Lista de Usuarios</h2>
@@ -136,5 +109,6 @@
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+@endsection    
 </body>
 </html>
