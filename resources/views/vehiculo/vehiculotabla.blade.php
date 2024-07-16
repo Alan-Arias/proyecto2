5aa6b521cb9bc8c2ff000041bb02999d4defdd05
Alan-Arias <74990048+Alan-Arias@users.noreply.github.com>
2024-07-16 02:38:39 -0400

    Update vehiculotabla.blade.php

--- resources/views/vehiculo/vehiculotabla.blade.php
+++ resources/views/vehiculo/vehiculotabla.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+b<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/49f6844fc6.js" crossorigin="anonymous"></script>
 <body>
-@extends('app2')
+@extends('appv2')
 
 @section('title', 'Inicio')
 
