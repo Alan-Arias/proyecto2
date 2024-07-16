23ebcee2860ac9fe27f97f37532857c28b31a7a6
Alan-Arias <74990048+Alan-Arias@users.noreply.github.com>
2024-07-16 00:53:22 -0400

    Update consultaDetVehic.blade.php

--- resources/views/vehiculo/consultaDetVehic.blade.php
+++ resources/views/vehiculo/consultaDetVehic.blade.php
@@ -38,22 +38,22 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/users"><i class="fas fa-car me-1"></i>Gestionar Usuarios</a>
+                    <a class="nav-link" href="{{ url('/users') }}"><i class="fas fa-car me-1"></i>Gestionar Usuarios</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/trabajadors"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Trabajadores</a>
+                    <a class="nav-link" href="{{ url('/trabajadors') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Trabajadores</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/servicios"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Servicios</a>
+                    <a class="nav-link" href="{{ url('/servicios') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Servicios</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/GestionarVehiculos"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Vehiculos</a>
+                    <a class="nav-link" href="{{ url('/GestionarVehiculos') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Vehiculos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/detalle"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Detalles Vehiculo</a>
+                    <a class="nav-link" href="{{ url('/detalle') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Detalles Vehiculo</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/reserva"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Reservas</a>
+                    <a class="nav-link" href="{{ url('/reserva') }}"><i class="fas fa-money-bill-alt me-1"></i>Gestionar Reservas</a>
                 </li>
             </ul>
         </div>
